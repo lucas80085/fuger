2c8b9919e734195fa4ffeb7fe701360f4bf4a66d
lucas80085 <88892063+lucas80085@users.noreply.github.com>
2021-11-03 18:26:15 +0100

    Update settings.py

--- fuger/settings.py
+++ fuger/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/portfolio/static/portfolio/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_ROOT = '/media/'
 
