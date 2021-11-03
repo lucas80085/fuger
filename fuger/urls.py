79315bd27780d0c60b94c37c9ecbec0d91937e1d
lucas80085 <88892063+lucas80085@users.noreply.github.com>
2021-11-03 17:57:53 +0100

    Update urls.py

--- fuger/urls.py
+++ fuger/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from portfolio import views
+from . import settings
+
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view)
+    path('', views.home_view),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
